ce5a4f408f0e3b547948e5d8053c5e6b95bcab59
senthil-prog <senthil@aiet.org.in>
2026-09-25 15:56:49 +0530

    unnecessary content removed

--- file1.py
+++ file1.py
@@ -1,10 +1,7 @@
 print("Addition", 8 + 7)
 print("Subtraction", 8 - 7)
-<<<<<<< HEAD
 print("Division", 8 / 4)
-=======
 print("Multiplication", 8 * 7)
->>>>>>> feature-branch
 for i in range(2):
-    print(i)
-i = 0
+    print(i, end="")
+
